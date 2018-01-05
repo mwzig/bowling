31be03b253e6cd0f5f2fabc16ea5880663b57d22
mwzig <mwziegler2014@gmail.com>
2018-01-05 15:24:06 -0500

    red test complete for addScoresAllSpares

--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -79,4 +79,22 @@ describe('ScoreCard', function () {
 		})
 	})
 
+	describe('#addScoresAllSpares', function() {
+		it('should return the count of 15', function() {
+		    testFrame1 = new Frame(1, "/", 0);
+		    testFrame2 = new Frame(2, "/", 0);
+		    testFrame3 = new Frame(3, "/", 0);
+		    testFrame4 = new Frame(4, "/", 0);
+		    testFrame5 = new Frame(5, "/", 0);
+		    testFrame6 = new Frame(6, "/", 0);
+		    testFrame7 = new Frame(7, "/", 0);
+		    testFrame8 = new Frame(8, "/", 0);
+		    testFrame9 = new Frame(9, "/", 0);
+		    testFrame10 = new Frame(1, "/", 5);
+		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
+		    testScoreCard = new ScoreCard(frameArray);
+		     assert.equal(testScoreCard.addFrameScores(), 50);
+		})
+	})
+
 })
