bef13b81293419b939f0ca56bd5d3843c6473b53
mwzig <mwziegler2014@gmail.com>
2018-01-05 15:24:31 -0500

    green test complete for addScoresAllSpares

--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -93,7 +93,7 @@ describe('ScoreCard', function () {
 		    testFrame10 = new Frame(1, "/", 5);
 		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
 		    testScoreCard = new ScoreCard(frameArray);
-		     assert.equal(testScoreCard.addFrameScores(), 50);
+		     assert.equal(testScoreCard.addFrameScores(), 150);
 		})
 	})
 
