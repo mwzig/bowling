73a647e4f9b3dde257eea4b8c162a2d41a9e888f
mwzig <mwziegler2014@gmail.com>
2018-01-05 14:45:10 -0500

    red test complete for add10FrameScoresNoSparesNoStrikesWithSecondRolls

--- scoreCard.js
+++ scoreCard.js
@@ -13,6 +13,7 @@ class ScoreCard {
 		var totalScore = 0;
 		for (var i = 0; i < this.frameArray.length; i++) {
 			totalScore += this.frameArray[i].getScore1();
+			totalScore += this.frameArray[i].getScore2();
 		}
 		return totalScore;
 	}
--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -16,9 +16,9 @@ describe('ScoreCard', function () {
 
 	describe('#addFrameScoresInitialTest', function() {
 		it('should return the count of 15', function() {
-		    testFrame1 = new Frame(5, 6, 7);
-		    testFrame2 = new Frame(5, 6, 7);
-		    testFrame3 = new Frame(5, 6, 7);
+		    testFrame1 = new Frame(5, 0, 0);
+		    testFrame2 = new Frame(5, 0, 0);
+		    testFrame3 = new Frame(5, 0, 0);
 		    var frameArray = [testFrame1, testFrame2, testFrame3]
 		    testScoreCard = new ScoreCard(frameArray);
 		     assert.equal(testScoreCard.addFrameScores(), 15);
@@ -37,6 +37,24 @@ describe('ScoreCard', function () {
 		    testFrame8 = new Frame(0, 0, 0);
 		    testFrame9 = new Frame(8, 0, 0);
 		    testFrame10 = new Frame(9, 0, 0);
+		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
+		    testScoreCard = new ScoreCard(frameArray);
+		     assert.equal(testScoreCard.addFrameScores(), 45);
+		})
+	})
+
+	describe('#add10FrameScoresNoSparesNoStrikesWithSecondRolls', function() {
+		it('should return the count of 15', function() {
+		    testFrame1 = new Frame(1, 8, 0);
+		    testFrame2 = new Frame(2, 7, 0);
+		    testFrame3 = new Frame(3, 6, 0);
+		    testFrame4 = new Frame(4, 5, 0);
+		    testFrame5 = new Frame(5, 4, 0);
+		    testFrame6 = new Frame(6, 3, 0);
+		    testFrame7 = new Frame(7, 2, 0);
+		    testFrame8 = new Frame(0, 9, 0);
+		    testFrame9 = new Frame(8, 1, 0);
+		    testFrame10 = new Frame(3, 4, 0);
 		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
 		    testScoreCard = new ScoreCard(frameArray);
 		     assert.equal(testScoreCard.addFrameScores(), 45);
