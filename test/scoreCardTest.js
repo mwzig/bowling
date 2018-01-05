775c481a70526d75c9342aa8f582ed84a196f6b5
mwzig <mwziegler2014@gmail.com>
2018-01-05 14:51:31 -0500

    red test complete for addScoresOneSpareNoStrikes

--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -61,5 +61,22 @@ describe('ScoreCard', function () {
 		})
 	})
 
+	describe('#addScoresOneSpareNoStrikes', function() {
+		it('should return the count of 15', function() {
+		    testFrame1 = new Frame(2, 8, 0);
+		    testFrame2 = new Frame(2, 7, 0);
+		    testFrame3 = new Frame(3, 6, 0);
+		    testFrame4 = new Frame(4, 5, 0);
+		    testFrame5 = new Frame(5, 4, 0);
+		    testFrame6 = new Frame(6, 3, 0);
+		    testFrame7 = new Frame(7, 2, 0);
+		    testFrame8 = new Frame(0, 9, 0);
+		    testFrame9 = new Frame(8, 1, 0);
+		    testFrame10 = new Frame(3, 4, 0);
+		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
+		    testScoreCard = new ScoreCard(frameArray);
+		     assert.equal(testScoreCard.addFrameScores(), 88);
+		})
+	})
 
 })
