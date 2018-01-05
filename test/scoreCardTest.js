39d813856233af87227455d5bcc8890ee24e77c1
mwzig <mwziegler2014@gmail.com>
2018-01-05 14:38:21 -0500

    red test complete for add10FrameScoresNoSparesNoStrikesNoSecondRoll

--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -13,6 +13,7 @@ describe('ScoreCard', function () {
 		     assert.equal(testScoreCard.getNbrOfFrames(), "3");
 		})
 	})
+
 	describe('#addFrameScoresInitialTest', function() {
 		it('should return the count of 15', function() {
 		    testFrame1 = new Frame(5, 6, 7);
@@ -23,4 +24,24 @@ describe('ScoreCard', function () {
 		     assert.equal(testScoreCard.addFrameScores(), 15);
 		})
 	})
+
+	describe('#add10FrameScoresNoSparesNoStrikesNoSecondRoll', function() {
+		it('should return the count of 15', function() {
+		    testFrame1 = new Frame(1, 0, 0);
+		    testFrame2 = new Frame(2, 0, 0);
+		    testFrame3 = new Frame(3, 0, 0);
+		    testFrame4 = new Frame(4, 0, 0);
+		    testFrame5 = new Frame(5, 0, 0);
+		    testFrame6 = new Frame(6, 0, 0);
+		    testFrame7 = new Frame(7, 0, 0);
+		    testFrame8 = new Frame(0, 0, 0);
+		    testFrame9 = new Frame(8, 0, 0);
+		    testFrame10 = new Frame(9, 0, 0);
+		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
+		    testScoreCard = new ScoreCard(frameArray);
+		     assert.equal(testScoreCard.addFrameScores(), 15);
+		})
+	})
+
+
 })
