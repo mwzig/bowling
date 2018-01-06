8904f23edd016e89edd4f87a0b89b9d01daf1da6
mwzig <mwziegler2014@gmail.com>
2018-01-05 20:00:06 -0500

    green test complete for addScoresNoHits

--- test/scoreCardTest.js
+++ test/scoreCardTest.js
@@ -194,7 +194,7 @@ describe('ScoreCard', function () {
 		    
 		    var frameArray = [testFrame1, testFrame2, testFrame3, testFrame4, testFrame5, testFrame6, testFrame7, testFrame8, testFrame9, testFrame10]
 		    testScoreCard = new ScoreCard(frameArray);
-		     assert.equal(testScoreCard.addFrameScores(), 67);
+		     assert.equal(testScoreCard.addFrameScores(), 0);
 		})
 	})
 
