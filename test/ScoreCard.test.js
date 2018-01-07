62b721b801a9402f86042547ec5b76c269a31d55
mwzig <mwziegler2014@gmail.com>
2018-01-06 20:22:31 -0500

    green test complete for ScoreCard.testUsingFrameMakerAllStrikes

--- test/ScoreCard.test.js
+++ test/ScoreCard.test.js
@@ -244,13 +244,13 @@ describe('ScoreCard', function () {
 		})
 	})
 	 	  
-	describe('#testUsingFrameMakerAllStikes', function() {
+	describe('#testUsingFrameMakerAllStrikes', function() {
 		it('should return the score of 90', function() {
 			testFrameMaker = new FrameMaker("X X X X X X X X X X X X");
 		    var frameArray = testFrameMaker.getFrames();
 		    var frame = frameArray[0];
 		    testScoreCard = new ScoreCard(frameArray);
-		    assert.equal(testScoreCard.addFrameScores(), 90);
+		    assert.equal(testScoreCard.addFrameScores(), 300);
 		})
 	})
 
